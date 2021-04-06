a8aa69174f664d21115963a8dded7da9f06e2516
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-04-06 18:03:16 +0200

    Shared keys for registry

--- src/commonTypes/Registry.ts
+++ src/commonTypes/Registry.ts
@@ -5,16 +5,22 @@ export namespace Registry {
     export interface TypeOptions {
         keys: Record<keyof any, any>,
         computed: Record<keyof any, any>,
+        keysShared: Record<keyof any, any>,
+        computedShared: Record<keyof any, any>,
         genID: { key: string, type: "increment" | "random" } | null
     }
 
     export interface Prototype<U, T extends TypeOptions = {
         keys: {},
         computed: {},
+        keysShared: {},
+        computedShared: {},
         genID: null
     }> extends PrototypeData {
         addKey<K extends keyof U, V = K extends keyof U ? U[K] : any>(key: K): Prototype<U, T & { keys: { [P in K]: V } }>
         addComputedKey<K extends string, V>(key: K, getter: (record: U) => V): Prototype<U, T & { computed: { [P in K]: V } }>
+        addKeyShared<K extends keyof U, V = K extends keyof U ? U[K] : any>(key: K): Prototype<U, T & { keysShared: { [P in K]: V } }>
+        addComputedKeyShared<K extends string, V>(key: K, getter: (record: U) => V): Prototype<U, T & { computedShared: { [P in K]: V } }>
         addIncrementID<K extends string = "id">(key?: K): Prototype<U, Omit<T, "genID"> & { genID: { key: K, type: "increment" } }>
         addRandomID<K extends string = "id">(key?: K): Prototype<U, Omit<T, "genID"> & { genID: { key: K, type: "random" } }>
         build(): Type<U, T>
@@ -24,13 +30,12 @@ export namespace Registry {
         new(): Instance<U, T>
     }
 
-    export type InstanceKeys<U, T extends TypeOptions> = {
-        [P in keyof T["keys"]]: Omit<Key<U, T["keys"][P]>, "register" | "unregister">
-    } & {
-            [P in keyof T["computed"]]: Omit<Key<U, T["keys"][P]>, "register" | "unregister">
-        } & (T["genID"] extends null ? {} : {
-            [P in NonNullable<T["genID"]>["key"]]: Omit<GeneratedKey<U>, "register" | "unregister">
-        })
+    export type InstanceKeys<U, T extends TypeOptions> =
+        { [P in keyof T["keys"]]: Omit<Key<U, T["keys"][P]>, "register" | "unregister"> }
+        & { [P in keyof T["computed"]]: Omit<Key<U, T["computed"][P]>, "register" | "unregister"> }
+        & { [P in keyof T["keysShared"]]: Omit<KeyShared<U, T["keysShared"][P]>, "register" | "unregister"> }
+        & { [P in keyof T["computedShared"]]: Omit<KeyShared<U, T["computedShared"][P]>, "register" | "unregister"> }
+        & (T["genID"] extends null ? {} : { [P in NonNullable<T["genID"]>["key"]]: Omit<GeneratedKey<U>, "register" | "unregister"> })
 
     export type KeyType<T> = T extends Omit<Key<any, infer U>, "register" | "unregister"> ? U : never
 
@@ -91,6 +96,50 @@ export namespace Registry {
         ) { }
     }
 
+    export class KeyShared<T = any, K = any> extends Key<T, K> {
+        public register(record: T, options: RegisterOptions) {
+            const key = this.getter(record, options)
+
+            const set = this.valueSetLookupMap.get(key)
+            if (set) set.add(record)
+            else this.valueSetLookupMap.set(key, new Set([record]))
+
+            this.reverseLookupMap.set(record, key)
+        }
+
+        public unregister(record: T) {
+            const key = this.findReverse(record)
+
+            const set = this.valueSetLookupMap.get(key)!
+            if (!set.delete(record)) throw new Error(`Tried to delete not yet registered record; key = "${key}"`)
+            if (set.size == 0) {
+                this.valueSetLookupMap.delete(key)
+            }
+
+            this.reverseLookupMap.delete(record)
+        }
+
+        public findAll(key: K) {
+            const set = this.valueSetLookupMap.get(key)
+            if (set == null) throw new Error(`Failed to find key "${toString(key)}"`)
+            return set as ReadonlySet<T>
+        }
+
+        public tryFindAll(key: K) {
+            return (this.valueSetLookupMap.get(key) ?? null) as ReadonlySet<T> | null
+        }
+
+        public tryFind(key: K) {
+            return this.tryFindAll(key)?.values().next().value as NonNullable<T> | null
+        }
+
+        public find(key: K) {
+            return this.findAll(key).values().next().value as NonNullable<T>
+        }
+
+        protected readonly valueSetLookupMap = new Map<K, Set<T>>()
+    }
+
     export class GeneratedKey<T = any> extends Key<T, string> {
 
         public nextID() {
@@ -119,6 +168,8 @@ export namespace Registry {
     export interface PrototypeData {
         keys: (keyof any)[],
         computed: { key: string, getter: (record: any) => any }[],
+        keysShared: (keyof any)[],
+        computedShared: { key: string, getter: (record: any) => any }[],
         genID: TypeOptions["genID"]
     }
 
@@ -126,13 +177,19 @@ export namespace Registry {
         genID?: string
     }
 
-    export function createPrototype(data: PrototypeData = { computed: [], keys: [], genID: null }): Prototype<any> {
+    export function createPrototype(data: PrototypeData = { computed: [], keys: [], genID: null, keysShared: [], computedShared: [] }): Prototype<any> {
         return {
             addKey(key) {
                 return createPrototype({ ...data, keys: [...data.keys, key] }) as any
             },
             addComputedKey(key, getter) {
                 return createPrototype({ ...data, computed: [...data.computed, { key, getter }] }) as any
+            },
+            addKeyShared(key) {
+                return createPrototype({ ...data, keysShared: [...data.keys, key] }) as any
+            },
+            addComputedKeyShared(key, getter) {
+                return createPrototype({ ...data, computedShared: [...data.computedShared, { key, getter }] }) as any
             },
             build() {
                 return function (this: Instance<any, any>) {
@@ -144,6 +201,14 @@ export namespace Registry {
 
                     for (const computed of data.computed) {
                         this.keys.push(new Key(computed.key, computed.key, computed.getter))
+                    }
+
+                    for (const key of data.keysShared) {
+                        this.keys.push(new KeyShared(key.toString(), key, v => v[key]))
+                    }
+
+                    for (const computed of data.computedShared) {
+                        this.keys.push(new KeyShared(computed.key, computed.key, computed.getter))
                     }
 
                     if (data.genID) {
@@ -166,7 +231,7 @@ export namespace Registry {
                     }
 
                     for (const key of this.keys) {
-                        this[key.index as any] = key
+                        this[key.index as any] = key as any
                     }
 
                     this[Symbol.iterator] = function* () {
