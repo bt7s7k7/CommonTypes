fcce5891fdc5e27df2a9d5c88cf1efdfa5168a66
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-04-06 18:03:26 +0200

    Ensure property function

---
+++ src/commonTypes/ensureProperty.ts
@@ -0,0 +1,4 @@
+export function ensureProperty<T extends Record<any, unknown>, K extends keyof T>(object: T, key: K, factory: () => T[K]): T[K] {
+    if (!(key in object)) object[key] = factory()
+    return object[key]
+}
