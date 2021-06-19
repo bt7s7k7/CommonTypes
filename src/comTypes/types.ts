533ac2d3aadc280c3e0e0fca1241afcbddd919c4
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-06-19 20:03:15 +0200

    Depromisify type

--- src/comTypes/types.ts
+++ src/comTypes/types.ts
@@ -1,2 +1,3 @@
 export type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never
 export type Constructor<T = any> = { new(...args: any[]): T }
+export type Depromisify<T> = T extends Promise<infer U> ? Depromisify<U> : T
