13123d0b98bc6793271e8b2305a89c6c7122615d
Branislav Trstenský <bt7s7k7@hotmail.sk>
2021-06-27 15:21:32 +0200

    Added type to filter only public properties

--- src/comTypes/types.ts
+++ src/comTypes/types.ts
@@ -1,3 +1,4 @@
 export type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never
 export type Constructor<T = any> = { new(...args: any[]): T }
 export type Depromisify<T> = T extends Promise<infer U> ? Depromisify<U> : T
+export type FilterPublic<T> = Pick<T, keyof T>
