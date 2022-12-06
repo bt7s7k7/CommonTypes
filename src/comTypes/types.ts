250950d74f869b8637a098a50a672fd1141b1528
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-12-06 18:07:45 +0100

    FilterBy type for property discriminated unions

--- src/comTypes/types.ts
+++ src/comTypes/types.ts
@@ -5,3 +5,4 @@ export type FilterPublic<T> = Pick<T, keyof T>
 export type Readwrite<T> = { -readonly [P in keyof T]: T[P] }
 export type Entry<T extends Record<any, any>> = [keyof T, T[keyof T]]
 export type Values<T extends Record<any, any>> = T[keyof T]
+export type FilterBy<T extends any, K extends keyof any, F extends any> = T extends { [P in K]: F } ? T : never
