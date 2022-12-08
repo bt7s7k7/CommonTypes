5c0631a34cfed99de7af66b61a416136e8499648
Branislav Trstenský <bt7s7k7@hotmail.sk>
2022-12-08 16:11:40 +0100

    Created REPL for development

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,7 +1,22 @@
-import { IDGen } from "../commonTypes/IDGen"
-import { range } from "../commonTypes/maths"
+import { start as replStart } from "repl"
+import { inspect, InspectOptions } from "util"
+import * as Graph from "../comTypes/Graph"
+import * as LinkedList from "../comTypes/LinkedList"
+import * as MultiMap from "../comTypes/MultiMap"
+import * as SortedArray from "../comTypes/SortedArray"
+import * as comTypes from "../comTypes/util"
 
-console.log("Random IDs: ")
-for (const _ of range(10)) {
-    console.log("  " + IDGen.random())
+const repl = replStart()
+Object.assign(repl.context, comTypes)
+Object.assign(repl.context, Graph)
+Object.assign(repl.context, LinkedList)
+Object.assign(repl.context, MultiMap)
+Object.assign(repl.context, SortedArray)
+
+function defineCustomInspect<T extends abstract new (...args: any) => any>(ctor: T, callback: (this: InstanceType<T>, depth: number, options: InspectOptions, _inspect: typeof inspect) => any) {
+    ctor.prototype[inspect.custom] = callback
 }
+
+defineCustomInspect(SortedArray.SortedArray, function (depth, options) {
+    return "SortedArray " + inspect([...this], options)
+})
