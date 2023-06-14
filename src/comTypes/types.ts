230b1115d73c6997561032a4935944c07328a968
Branislav Trstenský <bt7s7k7@hotmail.sk>
2023-06-14 15:26:10 +0200

    AbstractConstructor type

--- src/comTypes/types.ts
+++ src/comTypes/types.ts
@@ -1,4 +1,5 @@
 export type UnionToIntersection<T> = (T extends any ? (x: T) => any : never) extends (x: infer R) => any ? R : never
+export type AbstractConstructor<T = any> = abstract new (...args: any[]) => T
 export type Constructor<T = any> = { new(...args: any[]): T }
 export type Depromisify<T> = T extends Promise<infer U> ? Depromisify<U> : T
 export type FilterPublic<T> = Pick<T, keyof T>
