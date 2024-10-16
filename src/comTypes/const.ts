6c6b83e586e2337fc48f1eb1ca8d23e1f9303b6a
Branislav Trstenský <bt7s7k7@hotmail.sk>
2024-10-16 18:11:30 +0200

    Constants for empty set and map

--- src/comTypes/const.ts
+++ src/comTypes/const.ts
@@ -1,4 +1,8 @@
 /** Singleton empty array to prevent allocation when returning an empty result. */
 export const EMPTY_ARRAY: readonly never[] = []
+/** Singleton empty set to prevent allocation when returning an empty result. */
+export const EMPTY_SET: ReadonlySet<any> = new Set<any>()
+/** Singleton empty map to prevent allocation when returning an empty result. */
+export const EMPTY_MAP: ReadonlyMap<any, any> = new Map<any, any>()
 /** Singleton function returning its argument to prevent allocation for initializing default values. */
 export const NOOP = <T>(v: T) => v
